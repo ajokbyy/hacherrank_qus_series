647c4695b8bcc562ca98ad4a61e13fb35b8aa6e8
abhiraj <96772472+ajokbyy@users.noreply.github.com>
2024-10-23 13:22:35 +0530

    Add files via upload

---
+++ First.c
@@ -0,0 +1,57 @@
+// Hackerrenk Star 2 First Qus
+
+
+
+// Functions are a bunch of statements glued together. 
+// A function is provided with zero or more arguments,
+//  and it executes the statements on it. Based on the return type, it either returns nothing (void) or something.
+
+// The syntax for a function is
+
+
+// Syntex - 
+// return_type function_name(arg_type_1 arg_1, arg_type_2 arg_2, ...) {
+//     ...
+//     ...
+//     ...
+//     [if return_type is non void]
+//         return something of type `return_type`;
+// }
+
+// examp with parameter
+// int sum_of_four(int a, int b, int c, int d) {
+//     int sum = 0;
+//     sum += a;
+//     sum += b;
+//     sum += c;
+//     sum += d;
+//     return sum;
+// }
+
+// qus - >
+// Input Format
+
+// Input will contain four integers -  , one per line.
+
+// Output Format
+
+// Return the greatest of the four integers.
+// PS: I/O will be automatically handled.
+
+// Sampel input - 3
+// 4
+// 6
+// 5
+
+
+// Sample Output 
+// 6
+
+
+
+#include <stdio.h>
+
+
+int main(){
+    
+}
