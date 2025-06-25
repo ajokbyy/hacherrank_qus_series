3bd60b45e5f8dc3d21c4d526fc0a6641c4f822a9
abhiraj <96772472+ajokbyy@users.noreply.github.com>
2025-06-25 11:23:47 +0530

    Update binary_search_1.cpp

--- binary_search_1.cpp
+++ binary_search_1.cpp
@@ -57,3 +57,44 @@ public:
 };
 
 //binary search ---
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int lo = -1;
+        int ro = -1;
+        int n = nums.size();
+        int start = 0;
+        int end = n-1;
+        while(start<=end){
+            int mid = (start + end)/2;
+            if(nums[mid] > target){
+                end = mid -1;
+
+            }
+            else if(nums[mid] < target){
+                start = mid + 1;
+            }
+            else{
+                lo = mid;
+                end = mid -1;
+            }
+        }
+        start = 0;
+        end = n-1;
+        while(start<=end){
+            int mid = (start + end)/2;
+            if(nums[mid] > target){
+                end = mid -1;
+
+            }
+            else if(nums[mid] < target){
+                start = mid + 1;
+            }
+            else{
+                ro = mid;
+                start = mid + 1;
+            }
+        }
+        return {lo, ro};
+    }
+};
