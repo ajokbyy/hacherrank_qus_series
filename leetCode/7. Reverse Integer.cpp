16b112503bc5f27c2554c9e2e43a3b2446693354
abhiraj <96772472+ajokbyy@users.noreply.github.com>
2025-08-27 00:09:57 +0530

    Create 7. Reverse Integer.cpp

---
+++ leetCode/7. Reverse Integer.cpp
@@ -0,0 +1,45 @@
+------------------------------------------------------------------------------------------------------------------------------------------------
+  7. Reverse Integer
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+ 
+
+Example 1:
+
+Input: x = 123
+Output: 321
+Example 2:
+
+Input: x = -123
+Output: -321
+Example 3:
+
+Input: x = 120
+Output: 21
+ 
+
+Constraints:
+
+-231 <= x <= 231 - 1
+  ---------------------------------------------------------------------------
+  class Solution {
+public:
+    int reverse(int x) {
+        int ans = 0;
+        int rem;
+        while(x!=0){
+            rem = x%10;
+            x/=10;
+            if(ans>INT_MAX/10 || ans<INT_MIN/10) return 0;
+            ans = ans * 10 + rem;
+        }
+        return ans;
+    }
+};
