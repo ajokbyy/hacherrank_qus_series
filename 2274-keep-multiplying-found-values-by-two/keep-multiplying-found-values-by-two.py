2bdaecc365511a7f0709067fedf8aad342003e7e
abhiraj <96772472+ajokbyy@users.noreply.github.com>
2025-12-11 12:12:43 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (87.01%) - LeetSync

--- 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.py
+++ 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.py
@@ -10,3 +10,11 @@ class Solution:
             if not found:
                 return original
         
+
+# Optimal Approcah - we are using map set funtions here
+# fistly we are keeping every elemnet of nums in set
+#and then find which one is the original 
+# s = set(nums)
+# while original in s:
+#     original *= 2
+# return original
