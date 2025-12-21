dc59567812252ea77a300793bf973154033bdc3b
abhiraj <96772472+ajokbyy@users.noreply.github.com>
2025-12-21 21:09:15 +0530

    Time: 3 ms (25.46%) | Memory: 17.3 MB (98.73%) - LeetSync

---
+++ 2102-find-the-middle-index-in-array/find-the-middle-index-in-array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findMiddleIndex(self, nums: List[int]) -> int:
+
+        n = len(nums)
+        for i in range(n):
+            left_sum = sum(nums[:i])
+            right_sum = sum(nums[i+1:])
+        
+            if left_sum == right_sum:
+                return i
+            
+        return -1
+        
